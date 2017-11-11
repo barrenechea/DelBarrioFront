7e1c9224a386dd36698aeedfd41e54036687a384
Rodrigo Graf <rodrigo.graf.m@gmail.com>
2017-11-10 22:01:02 -0300

    No era Merge, era Master. Falta la validaciòn de todos los formularios. Planif. 12-11-17

--- src/components/clients/EditClient.vue
+++ src/components/clients/EditClient.vue
@@ -149,24 +149,6 @@
   }
 </script>
 
-<style>
-/*
-  .btn {
-    background-color: #372d57;
-  }
-  .btn:hover {
-    background-color: #3c4c78;
-  }
-  .btn:focus {
-    background-color: #484456;
-  }
-  .input-field input[type=text]:focus + label {
-    color: #888 !important;
-  }
-  .input-field input[type=text]:focus {
-    border-bottom: 1px solid #888 !important;
-    box-shadow: 0 1px 0 0 #888 !important;
-  }
 
-*/
+<style>
 </style>
