7007df97f9f4eb950599fe840e36f2ba687fbab1
Dominique Bustos <do.bustos@alumnos.duoc.cl>
2017-12-10 20:31:03 -0300

    index

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h2><nuxt-link to="/busqueda-avanzada">Búsqueda avanzada</nuxt-link></h2>
-  </div>
+  <section id="publicacion" class="container-fluid">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1 fondo-beige">
+          <form @submit.prevent="validateBeforeSubmit">
+            <div class="form-group margin-top">
+              <input class="form-control input-lg" placeholder="¿Qué buscas?"/>
+            </div>
+          </form>
+          <center>
+          <img class="margin-top" style="text-align: center" :src="'https://i.ytimg.com/vi/vUQUDS-TL6Q/hqdefault.jpg'"></img>
+          <h2><nuxt-link to="/busqueda-avanzada">Manual de usuario</nuxt-link></h2>
+          <h2><nuxt-link to="/busqueda-avanzada">Búsqueda avanzada</nuxt-link></h2>
+          </center>
+          
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
